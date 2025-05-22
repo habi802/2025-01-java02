e19b5413d68b538e3594e3f134d1400128b7d1ae
habi802 <shb8002@gmail.com>
2025-05-22 17:58:34 +0900

    자바 기본, 정수/문자 타입

---
+++ src/ch02/sec02/CharExample.java
@@ -0,0 +1,20 @@
+package ch02.sec02;
+
+public class CharExample {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+
+        System.out.printf("ch1: %c, %d\n", ch1, (int)ch1);
+        System.out.printf("66: %c, %d\n", 66, 66);
+
+        char ch2 = 68;
+        System.out.printf("ch2: %c, %d\n", ch2, (int)ch2);
+
+        char ch3 = '가';
+        System.out.printf("ch3: %c, %d\n", ch3, (int) ch3);
+
+        String str = "Hello";
+        char[] strArr = str.toCharArray();
+        System.out.println(strArr.length);
+    }
+}
