d8b13b31f15836287ff1ff3afd09d702d0176ce8
habi802 <shb8002@gmail.com>
2025-06-12 18:02:21 +0900

    상속

---
+++ src/ch07/sec02/Phone.java
@@ -0,0 +1,39 @@
+package ch07.sec02;
+
+public class Phone {
+    private String model;
+    private String color;
+
+    //public Phone() {}
+
+    public Phone(String model, String color) {
+        //super();
+        System.out.println("Phone 생성자 호출!!!!!!!");
+        this.model = model;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void bell() {
+        System.out.println("벨이 울립니다.");
+    }
+
+    public void sendSms(String message) {
+        System.out.println("본인: " + message);
+    }
+
+    public void recevieSms(String message) {
+        System.out.println("상대방: " + message);
+    }
+
+    public void hangUp() {
+        System.out.println("전화를 끊습니다.");
+    }
+}
